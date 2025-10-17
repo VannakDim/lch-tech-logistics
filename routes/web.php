569911958f4b06f597135fdd0b60e24ea89cf439
Vannak Dim <vannakhannah@gmail.com>
2025-10-17 15:09:08 +0700

    fix lch host

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ItemsCategoriesController;
+use App\Http\Controllers\ItemsController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,48 +19,48 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return view('stockin.index');
     })->name('stockin');
 
-    Route::get('/categories/{category}', [\App\Http\Controllers\ItemsCategoriesController::class, 'show'])
+    Route::get('/categories/{category}', [ItemsCategoriesController::class, 'show'])
         ->name('categories.show');
 
-    Route::get('/items/{item}', [\App\Http\Controllers\ItemsController::class, 'show'])
+    Route::get('/items/{item}', [ItemsController::class, 'show'])
         ->name('items.show');
 
     // Categories routes
-    Route::get('/categories', [\App\Http\Controllers\ItemsCategoriesController::class, 'index'])
+    Route::get('/categories', [ItemsCategoriesController::class, 'index'])
         ->name('categories.index');
 
-    Route::get('/categories/create', [\App\Http\Controllers\ItemsCategoriesController::class, 'create'])
+    Route::get('/categories/create', [ItemsCategoriesController::class, 'create'])
         ->name('categories.create');
 
-    Route::post('/categories', [\App\Http\Controllers\ItemsCategoriesController::class, 'store'])
+    Route::post('/categories', [ItemsCategoriesController::class, 'store'])
         ->name('categories.store');
 
-    Route::get('/categories/{category}/edit', [\App\Http\Controllers\ItemsCategoriesController::class, 'edit'])
+    Route::get('/categories/{category}/edit', [ItemsCategoriesController::class, 'edit'])
         ->name('categories.edit');
 
-    Route::put('/categories/{category}', [\App\Http\Controllers\ItemsCategoriesController::class, 'update'])
+    Route::put('/categories/{category}', [ItemsCategoriesController::class, 'update'])
         ->name('categories.update');
 
-    Route::delete('/categories/{category}', [\App\Http\Controllers\ItemsCategoriesController::class, 'destroy'])
+    Route::delete('/categories/{category}', [ItemsCategoriesController::class, 'destroy'])
         ->name('categories.destroy');
 
     // Items routes
-    Route::get('/items', [\App\Http\Controllers\ItemsController::class, 'index'])
+    Route::get('/items', [ItemsController::class, 'index'])
         ->name('items.index');
 
-    Route::get('/items/create', [\App\Http\Controllers\ItemsController::class, 'create'])
+    Route::get('/items/create', [ItemsController::class, 'create'])
         ->name('items.create');
 
-    Route::post('/items', [\App\Http\Controllers\ItemsController::class, 'store'])
+    Route::post('/items', [ItemsController::class, 'store'])
         ->name('items.store');
 
-    Route::get('/items/{item}/edit', [\App\Http\Controllers\ItemsController::class, 'edit'])
+    Route::get('/items/{item}/edit', [ItemsController::class, 'edit'])
         ->name('items.edit');
 
-    Route::put('/items/{item}', [\App\Http\Controllers\ItemsController::class, 'update'])
+    Route::put('/items/{item}', [ItemsController::class, 'update'])
         ->name('items.update');
 
-    Route::delete('/items/{item}', [\App\Http\Controllers\ItemsController::class, 'destroy'])
+    Route::delete('/items/{item}', [ItemsController::class, 'destroy'])
         ->name('items.destroy');
 });
 
